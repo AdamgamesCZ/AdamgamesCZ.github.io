0b164fb2d0c9c5c71723ef38e8b3ed3938047888
AdamgamesCZ <adam.cermak.5@seznam.cz>
2024-11-10 01:57:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function changeContent(section) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(sec => sec.style.display = 'none'); // Skryj všechny sekce
+
+    // Zobraz sekci podle ID
+    if (section === 'logo') {
+        document.getElementById('logo-content').style.display = 'block';
+        document.getElementById('logo-img').style.display = 'block'; // Zobraz obrázek v logo-content
+    } else {
+        document.getElementById(`${section}-content`).style.display = 'block';
+        document.getElementById('logo-img').style.display = 'none'; // Skryj obrázek v ostatních sekcích
+    }
+
+    // Aktualizace aktivního odkazu
+    document.querySelectorAll('nav a').forEach(link => link.classList.remove('active'));
+    document.getElementById(section).classList.add('active');
+}
